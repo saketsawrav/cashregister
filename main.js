247c5023c169053de103428027107258a40e14a5
ssawrav <s.sawrav@gmail.com>
2022-06-17 14:29:25 -0400

    JS:Add another check to see if totalAmount is positve
    - If not show an error message & don't proceed further
    
    Signed-off-by: ssawrav <s.sawrav@gmail.com>

--- main.js
+++ main.js
@@ -9,6 +9,15 @@ checkButton.addEventListener('click', clickHandler)
 function clickHandler () {
     if (cashGiven.value > 0 && billAmount.value > 0) {
         console.log("Amount is valid")
+        // Validate if Cash given by the customer is bigger then the Bill Amount
+        if ( cashGiven.value >= billAmount.value) {
+            console.log("cash given is valid")
+            var totalAmount = cashGiven.value - billAmount.value
+            console.log(totalAmount)
+        } else {
+            errorMessage.innerText = "The Amount given by the customer is not enough"
+            console.log("cash given by customer is not enough to proceed further")
+        }
     } else {
         errorMessage.innerText = "The Amount entered is invalid!!"
         console.log("The amount is invalid!!")
