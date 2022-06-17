c1959e29d7ed5d14179fb827e80a24f576af2a27
ssawrav <s.sawrav@gmail.com>
2022-06-17 19:10:05 -0400

    JS:cleanup:remove logging
    - No need for it now
    
    Signed-off-by: ssawrav <s.sawrav@gmail.com>

--- main.js
+++ main.js
@@ -9,24 +9,15 @@ checkButton.addEventListener('click', clickHandler)
 
 function clickHandler() {
     if (Number(cashGiven.value) > 0 && Number(billAmount.value) > 0) {
-        console.log("Amount is valid")
         // Validate if Cash given by the customer is bigger then the Bill Amount
         if (Number(cashGiven.value) >= Number(billAmount.value)) {
-            console.log(cashGiven.value >= billAmount.value)
-            console.log(cashGiven.value + " , " + billAmount.value)
-            console.log("cash given is valid")
             var totalAmount = cashGiven.value - billAmount.value
             calculateChange(totalAmount)
-            console.log(totalAmount)
         } else {
-            console.log(cashGiven.value >= billAmount.value)
-            console.log(cashGiven.value + " , " + billAmount.value)
             errorMessage.innerText = "The Amount given by the customer is not enough"
-            console.log("cash given by customer is not enough to proceed further")
         }
     } else {
         errorMessage.innerText = "The Amount entered is invalid!!"
-        console.log("The amount is invalid!!")
     }
 }
 
